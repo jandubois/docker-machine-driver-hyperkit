ade2b4ebfdd40a177926c1a5c0ac043e360dab4b
Jan Dubois <jan.dubois@suse.com>
2021-04-19 13:47:17 -0700

    Add executable path to PATH to use driver from same directory
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"syscall"
 
 	"github.com/docker/machine/libmachine/drivers/plugin"
@@ -38,6 +39,21 @@ func main() {
 		fmt.Fprintf(os.Stderr, "cannot drop privileges: %v", err)
 		os.Exit(1)
 	}
+
+	// Add the directory name of the current executable to the front of the PATH to load
+	// the driver from the same directory (the current executable may be a symlink to the
+	// driver, living in a different directory).
+	executable, err := os.Executable()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot determine absolute path to current executable: %v", err)
+		os.Exit(1)
+	}
+	executable, err = filepath.EvalSymlinks(executable)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot evaluate symlinks in path to current executable: %v", err)
+		os.Exit(1)
+	}
+	os.Setenv("PATH", os.ExpandEnv(fmt.Sprintf("%s:$PATH", filepath.Dir(executable))))
 
 	cmd.Execute()
 }
